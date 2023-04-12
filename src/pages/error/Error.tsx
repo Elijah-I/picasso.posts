ec5a2075cadfb605b29e0c669be989cd09f3e86e
Elijah <elijah.ivanik@inbox.ru>
2023-04-12 12:24:28 +0300

    feat: implement Error page

--- src/pages/error/Error.tsx
+++ src/pages/error/Error.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import styles from './Error.module.scss';
 
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <div className={styles.error}>
+      <div className={styles.error__content}>Page not found</div>
+    </div>
+  );
 };
 
 export default Error;
