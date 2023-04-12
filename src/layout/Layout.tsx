5377f85bce016db0084e00bcecdd2fe825cc8f8f
Elijah <elijah.ivanik@inbox.ru>
2023-04-12 15:41:56 +0300

    refactor: page layout styles

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../components/header/Header';
+import styles from './Layout.module.scss';
 
 const Layout = () => {
   return (
-    <>
+    <div className={styles.container}>
       <Header />
       <Outlet />
-    </>
+    </div>
   );
 };
 
