cfa62abbc5856b9a4e3d4feed39c184780e2b52c
Elijah <elijah.ivanik@inbox.ru>
2023-04-12 12:04:02 +0300

    feat: implement navigation

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import styles from './Header.module.scss';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
-  return <div className={styles.header}>Header</div>;
+  const home = import.meta.env.BASE_URL;
+
+  return (
+    <div className={styles.header}>
+      <div className={styles.header__content}>
+        <NavLink end to={home} className={styles.header__link}>
+          home
+        </NavLink>
+        <NavLink to={`${home}/posts`} className={styles.header__link}>
+          posts
+        </NavLink>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
